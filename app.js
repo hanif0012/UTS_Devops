98bf4d5cb6b7c193ec1236a3b45f0c1ddbca9e65
hanif0012 <hanifwildan420@gmail.com>
2025-05-20 01:12:01 +0700

    Menambahkan JenkinsFile

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
